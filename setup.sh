39a7faabcb4a51e17fef63a3ee7d514d5e637f19
Vito Botta <vito@botta.me>
2020-01-18 22:32:25 +0200

    Ensure apt-get waits until it's not used by another process

--- setup.sh
+++ setup.sh
@@ -3,7 +3,11 @@
 TOKEN="$1"
 WHITELIST_S="$2"
 
-apt install -y jq ufw fail2ban
+curl -o /usr/local/sbin/apt-get https://raw.githubusercontent.com/vitobotta/hetzner-cloud-init/master/apt-get
+
+chmod +x /usr/local/sbin/apt-get
+
+apt-get install -y jq ufw fail2ban
 
 curl -o /usr/local/bin/update-ufw.sh https://raw.githubusercontent.com/vitobotta/hetzner-cloud-init/master/update-ufw.sh
 
