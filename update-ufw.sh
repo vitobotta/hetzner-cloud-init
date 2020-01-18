94b89ed0d6f2d5a6d4dfa7aec5a0224e08ac0e35
Vito Botta <vito@botta.me>
2020-01-18 19:30:11 +0200

    Deny access to removed nodes

--- update-ufw.sh
+++ update-ufw.sh
@@ -13,10 +13,31 @@ for IP in "${WHITELIST[@]}"; do
   ufw allow from "$IP"
 done
 
-NODE_IPS=( $(curl -H 'Accept: application/json' -H "Authorization: Bearer ${TOKEN}" 'https://api.hetzner.cloud/v1/servers' | jq -r '.servers[].public_net.ipv4.ip') )
+NEW_NODE_IPS=( $(curl -H 'Accept: application/json' -H "Authorization: Bearer ${TOKEN}" 'https://api.hetzner.cloud/v1/servers' | jq -r '.servers[].public_net.ipv4.ip') )
 
-for IP in "${NODE_IPS[@]}"; do
+touch /etc/current_node_ips
+cp /etc/current_node_ips /etc/old_node_ips
+echo "" > /etc/current_node_ips
+
+for IP in "${NEW_NODE_IPS[@]}"; do
   ufw allow from "$IP"
+  echo "$IP" >> /etc/current_node_ips
+done
+
+IFS=$'\r\n' GLOBIGNORE='*' command eval 'OLD_NODE_IPS=($(cat /etc/old_node_ips))'
+
+REMOVED=()
+for i in "${OLD_NODE_IPS[@]}"; do
+  skip=
+  for j in "${NEW_NODE_IPS[@]}"; do
+    [[ $i == $j ]] && { skip=1; break; }
+  done
+  [[ -n $skip ]] || REMOVED+=("$i")
+done
+declare -p REMOVED
+
+for IP in "${REMOVED[@]}"; do
+  ufw deny from "$IP"
 done
 
 ufw allow from 10.43.0.0/16
