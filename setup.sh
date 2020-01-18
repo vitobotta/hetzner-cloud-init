25bbb5db0ffc8870ffdc0eb6232546fecffee221
Vito Botta <vito@botta.me>
2020-01-18 23:14:29 +0200

    Do not update all the rules each time, but just the nodes ips

--- setup.sh
+++ setup.sh
@@ -13,6 +13,22 @@ curl -o /usr/local/bin/update-ufw.sh https://raw.githubusercontent.com/vitobotta
 
 chmod +x /usr/local/bin/update-ufw.sh
 
+ufw allow proto tcp from any to any port 22,80,443
+
+ufw -f enable
+
+IFS=', ' read -r -a WHITELIST <<< "$WHITELIST_S"
+
+for IP in "${WHITELIST[@]}"; do
+  ufw allow from "$IP"
+done
+
+ufw allow from 10.43.0.0/16
+ufw allow from 10.42.0.0/16
+
+ufw -f default deny incoming
+ufw -f default allow outgoing
+
 /usr/local/bin/update-ufw.sh ${TOKEN} ${WHITELIST_S}
 
 cat <<EOF >> /etc/crontab
--- update-ufw.sh
+++ update-ufw.sh
@@ -3,16 +3,6 @@
 TOKEN="$1"
 WHITELIST_S="$2"
 
-ufw allow proto tcp from any to any port 22,80,443
-
-ufw -f enable
-
-IFS=', ' read -r -a WHITELIST <<< "$WHITELIST_S"
-
-for IP in "${WHITELIST[@]}"; do
-  ufw allow from "$IP"
-done
-
 NEW_NODE_IPS=( $(curl -H 'Accept: application/json' -H "Authorization: Bearer ${TOKEN}" 'https://api.hetzner.cloud/v1/servers' | jq -r '.servers[].public_net.ipv4.ip') )
 
 touch /etc/current_node_ips
@@ -39,9 +29,3 @@ declare -p REMOVED
 for IP in "${REMOVED[@]}"; do
   ufw deny from "$IP"
 done
-
-ufw allow from 10.43.0.0/16
-ufw allow from 10.42.0.0/16
-
-ufw -f default deny incoming
-ufw -f default allow outgoing
