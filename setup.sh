255c25796fc536001ca6c943db35c8fe2fcc84cc
Vito Botta <vito@botta.me>
2020-06-16 14:12:35 +0300

    Install packages with Cloud init

--- setup.sh
+++ setup.sh
@@ -33,14 +33,6 @@ sed -i 's/[#]*PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/s
 
 systemctl restart sshd
 
-curl -o /usr/local/sbin/apt-get https://raw.githubusercontent.com/vitobotta/hetzner-cloud-init/master/apt-get
-
-chmod +x /usr/local/sbin/apt-get
-
-export DEBIAN_FRONTEND=noninteractive
-apt-get update
-apt-get install -y jq ufw fail2ban
-
 curl -o /usr/local/bin/update-config.sh https://raw.githubusercontent.com/vitobotta/hetzner-cloud-init/master/update-config.sh
 
 chmod +x /usr/local/bin/update-config.sh
